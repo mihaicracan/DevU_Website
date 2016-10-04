c748071b9479ddeaebb43da005829b2966f4ed1c
mihaigeorge <mihaigeorge.c@gmail.com>
2016-10-04 08:25:38 +0300

    Removed Unused Scripts

--- resources/views/presentation.blade.php
+++ resources/views/presentation.blade.php
@@ -196,11 +196,11 @@
 
         <div id="footer-wrapper">2016 &copy; DevU Technologies SRL</div>
 
-        <script src="https://maps.googleapis.com/maps/api/js"></script>
+        <!-- <script src="https://maps.googleapis.com/maps/api/js"></script> -->
         <script type="text/javascript" src="{{ url('js/lib.js?v=1.1.0') }}"></script>
         <script type="text/javascript" src="{{ url(elixir('js/app.js')) }}"></script>
 
-        <script>
+        <!-- <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -208,26 +208,6 @@
 
           ga('create', 'UA-78896263-1', 'auto');
           ga('send', 'pageview');
-        </script>
-
-        <!-- Hotjar Tracking Code for http://mihaicracan.com -->
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:230115,hjsv:5};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
-
-
-        <script>
-          window.intercomSettings = {
-            app_id: "yhh0y2k6"
-          };
-        </script>
-        <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/yhh0y2k6';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+        </script> -->
     </body>
 </html>
